2c123470cf9ac1f443cfe6436c603ee38102cb86
Carlos Coello <carlos.coello05@outlook.com>
2017-08-01 17:54:12 -0500

    changes

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.listen(port, () => {
+  console.log('Listening on port ' + port);
+});
+
 const corsOptions = {
-  origin: 'https://young-sea-76737.herokuapp.com/'
+  origin: 'https://young-sea-76737.herokuapp.com/',
 }
 
 app.use( cors(corsOptions));
 app.use( express.static(__dirname + '/dist') );
-
-app.listen(port, () => {
-  console.log('Listening on port ' + port);
-});
