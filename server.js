24681d22348a4f51c936b374cb9d56e161c2dd63
Carlos Coello <carlos.coello05@outlook.com>
2017-08-01 17:50:51 -0500

    changes

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 
-app.use( cors());
+const corsOptions = {
+  origin: 'https://young-sea-76737.herokuapp.com/'
+}
+
+app.use( cors(corsOptions));
 app.use( express.static(__dirname + '/dist') );
 
 app.listen(port, () => {
