0e8c6a8e2baa9460374cecd86cf1a83603613407
Carlos Coello <carlos.coello05@outlook.com>
2017-08-01 18:12:23 -0500

    changes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -7,4 +8,9 @@ app.listen(port, () => {
   console.log('Listening on port ' + port);
 });
 
+const corsOptions = {
+  origin: 'https://api.darksky.net/forecast/8faa7b95e3a14d8e2f746aba2c2cbecf/',
+}
+
+app.use( cors(corsOptions));
 app.use( express.static(__dirname + '/dist') );
