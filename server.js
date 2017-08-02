892cf52385aa0f4e582e4ac4430d0cd791f9dfac
Carlos Coello <carlos.coello05@outlook.com>
2017-08-02 02:23:52 -0500

    changes

--- server.js
+++ server.js
@@ -8,4 +8,5 @@ app.listen(port, () => {
   console.log('Listening on port ' + port);
 });
 
+app.use( cors({origin: 'https://api.darksky.net/forecast/8faa7b95e3a14d8e2f746aba2c2cbecf/'}))
 app.use( express.static(__dirname + '/dist') );
