8e00057cfb6b2d5a689c487a270195963f159272
Carlos Coello <carlos.coello05@outlook.com>
2017-08-02 03:01:39 -0500

    changes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -8,3 +9,5 @@ app.listen(port, () => {
 });
 
 app.use( express.static(__dirname + '/dist') );
+
+app.use( cors());
