cb2601df6eb4a06b3bca418bc0a959a46c6948ac
Carlos Coello <carlos.coello05@outlook.com>
2017-08-02 03:29:44 -0500

    changes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -8,3 +9,15 @@ app.listen(port, () => {
 });
 
 app.use( express.static(__dirname + '/dist') );
+app.use( cors());
+
+const forceSSL = function(){
+  return function(req, res, next){
+    if(req.headers['x-forwarded-proto'] !== 'https'){
+      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
