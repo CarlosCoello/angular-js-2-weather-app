397e9ba8d6f26b93b29f4284d1d0e1dd87d7b28e
Carlos Coello <carlos.coello05@outlook.com>
2017-08-02 02:16:12 -0500

    changes

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -9,8 +9,13 @@ import { WeatherService } from '../weather.service';
 })
 export class SearchComponent implements OnInit {
 @ViewChild('f') form:any;
-encodedAddress: string;
 search: string;
+address: string;
+lat: string;
+lng: string;
+results = [];
+obj;
+temperature: string;
 
   constructor(private weather: WeatherService) { }
 
@@ -18,12 +23,18 @@ search: string;
   }
 
   searchWeather(){
-    this.weather.geocodeAddress(this.search).then( (res) => res).catch( (error) => console.log(error));
-
-    console.log(this.weather.weatherRes);
-
+    this.weather.geocodeAddress(this.search).subscribe( (data) => {
+      this.address = data[0].address;
+      this.lat = data[0].lat;
+      this.lng = data[0].lng;
+      this.weather.darkSky(this.lat, this.lng).subscribe( (data) => {
+        this.temperature = data.currently.temperature;
+        this.obj = { address: this.address, temperature: this.temperature };
+        this.results.push(this.obj);
+      });
+    });
     this.form.reset();
-
   }
+
 
 }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,61 +1,41 @@
 import { Injectable } from '@angular/core';
 import { Http, HttpModule } from '@angular/http';
-import 'rxjs/add/operator/topromise';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+
+class GoogleFetch{
+    constructor(
+      public address:string,
+      public lat: string,
+      public lng: string
+    ){}
+}
 
 @Injectable()
 export class WeatherService {
-weatherRes: Object[];
 encodedAddress: string;
 apiUrl: string;
-address: string;
-lat: number;
-lng: number;
 apiDarkSky: string;
-temperature: number;
 
-  constructor(private http: Http) {
-    this.weatherRes = [];
+  constructor(private http: Http) {}
+
+   geocodeAddress(address: string): Observable<GoogleFetch[]>{
+     this.encodedAddress = encodeURIComponent(address);
+     this.apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${this.encodedAddress}`;
+     return this.http.get(this.apiUrl).map( res => {
+       return res.json().results.map( x => {
+         return new GoogleFetch(
+           x.formatted_address,
+           x.geometry.location.lat,
+           x.geometry.location.lng
+         );
+       });
+     });
    }
 
-  geocodeAddress = (address: string) => {
-
-    let promise = new Promise( (resolve, reject ) => {
-
-      this.encodedAddress = encodeURIComponent(address);
-      this.apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${this.encodedAddress}`;
-      this.http.get(this.apiUrl)
-            .toPromise()
-            .then( (res) => {
-              this.address = res.json().results[0].formatted_address;
-              this.lat = res.json().results[0].geometry.location.lat;
-              this.lng = res.json().results[0].geometry.location.lng;
-              resolve( this.darkSky() );
-            }, (msg) => {
-              reject();
-            });
-    });
-
-    return promise;
-
-  };
-
-  darkSky = () => {
-    let  promise = new Promise( (resolve, reject) => {
-
-      this.apiDarkSky = `https://api.darksky.net/forecast/8faa7b95e3a14d8e2f746aba2c2cbecf/${this.lat},${this.lng}`;
-      this.http.get(this.apiDarkSky)
-          .toPromise()
-          .then( (res) => {
-            this.temperature = res.json().currently.temperature;
-            resolve(this.weatherRes.push({ address: this.address, temperature: this.temperature }));
-          }, (msg) => {
-            reject('Error happened, verify http request url is correct or that the address you typed is a true address');
-          })
-    })
-
-    return promise;
-  };
-
-
+   darkSky(lat, lng){
+     this.apiDarkSky = `https://api.darksky.net/forecast/8faa7b95e3a14d8e2f746aba2c2cbecf/${lat},${lng}`;
+     return this.http.get(this.apiDarkSky).map( res => res.json());
+   }
 
 }
