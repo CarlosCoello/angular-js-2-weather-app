d82f7afef53a57bd38a9fdb23825e76ce7aad653
Carlos Coello <carlos.coello05@outlook.com>
2017-08-02 02:43:40 -0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { JsonpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
@@ -15,7 +15,7 @@ import { WeatherService } from '../app/weather.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    JsonpModule
   ],
   providers: [WeatherService],
   bootstrap: [AppComponent]
